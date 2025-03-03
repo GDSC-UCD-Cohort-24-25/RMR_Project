f71a2679551aad7442f6d9f2fbb65ca6385deeb5
Noah Im <noahim03@gmail.com>
2025-03-02 21:29:08 -0800

    Add files via upload

---
+++ data/restaurantData.tsx
@@ -0,0 +1,26 @@
+export const restaurants = [
+  {
+    id: '0', // Ensure the id is a string
+    name: "Honey D Cafe",
+    rating: "4.1/5",
+    address: "213 E St, Davis, CA 95616",
+    menuAvailable: true,
+    imageUrl: "https://cdn.builder.io/api/v1/image/assets/TEMP/175e346332a040eb39a083c25ac867d81fbbe23fed95f978b0bc11ab1c0a695e?placeholderIfAbsent=true&apiKey=06b242cc5f094243a48af7e24ba49b66",
+  },
+  {
+    id: '1', // Ensure the id is a string
+    name: "Cajun Feast",
+    rating: "3.9/5",
+    address: "206 3rd St, Davis, CA 95616",
+    menuAvailable: true,
+    imageUrl: "https://cdn.builder.io/api/v1/image/assets/TEMP/7e636cad2b89a287b5b95b0076146250248b1ad6ebbf314175c0426d4ffa3320?placeholderIfAbsent=true&apiKey=06b242cc5f094243a48af7e24ba49b66",
+  },
+  {
+    id: '2', // Ensure the id is a string
+    name: "Sit Lo Saigon",
+    rating: "4.5/5",
+    address: "424 G St, Davis, CA 95616",
+    menuAvailable: true,
+    imageUrl: "https://cdn.builder.io/api/v1/image/assets/TEMP/5188446b45057fd40c1eb2423a428ee66da3fe420912e1a66526f6132e078f33?placeholderIfAbsent=true&apiKey=06b242cc5f094243a48af7e24ba49b66",
+  },
+];
