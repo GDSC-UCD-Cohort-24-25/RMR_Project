ef9198164739525a4303a10fc04978f7e7dcd68c
Noah Im <noahim03@gmail.com>
2025-03-06 18:47:11 -0800

    Add files via upload

---
+++ app/signin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+"use client"; // Required for client-side interactions
+import { AuthPage } from "../components/AuthPage";
+
+export default function SignInPage() {
+  return <AuthPage />;
+}
