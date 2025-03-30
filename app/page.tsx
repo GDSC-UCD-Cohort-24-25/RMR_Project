8e321cfb6283af3fec38d54dc9d15f884180b41d
Noah Im <noahim03@gmail.com>
2025-03-29 23:45:33 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { NavButton } from './components/NavButton';
+import { RestaurantList } from './components/RestaurantList';
+import { Header } from './components/Header';
+
+
+export default function RestaurantPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <Header />
+      <main className="flex-1 flex flex-col items-center px-4 sm:px-8 lg:px-20">
+        <div className="w-full max-w-4xl text-center py-14">
+          <h1 className="nothing-you-could-do-regular text-6xl font-bold tracking-tighter text-black mb-4">
+            RateMyRestaurant
+          </h1>
+          <div className="w-full h-0.5 bg-black/20 shadow-sm" />
+        </div>
+
+        {/* Restaurant list */}
+        <section className="w-full max-w-6xl mt-1">
+          <RestaurantList />  
+        </section>
+      </main>
+    </div>
+  );
+}
+      
+      
+   
